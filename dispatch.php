c74855908cd79b2468e9a4443446955bc0b0dd19
Prakash Gyawali <hsageyolk@gmail.com>
2026-07-15 22:11:46 +0545

    Add direct admin route dispatch in dispatch.php to bypass Router for broken 404 admin pages

--- dispatch.php
+++ dispatch.php
@@ -9,7 +9,22 @@ require_once __DIR__ . '/models/Inquiry.php';
 require_once __DIR__ . '/models/Post.php';
 require_once __DIR__ . '/models/Category.php';
 
-$route = ltrim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/') ?: '/';
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?? '/';
+$uri = rtrim($uri, '/');
+
+$directRoutes = [
+    '/admin/media' => __DIR__ . '/admin/media-handler.php',
+    '/admin/subscribers' => __DIR__ . '/admin/subscribers-handler.php',
+    '/admin/categories' => __DIR__ . '/admin/categories-handler.php',
+    '/admin/settings' => __DIR__ . '/admin/settings-handler.php',
+];
+if (isset($directRoutes[$uri])) {
+    http_response_code(200);
+    require $directRoutes[$uri];
+    exit;
+}
+
+$route = ltrim($uri, '/') ?: '/';
 
 Router::registerRoutes();
 Router::dispatch($route, $_SERVER['REQUEST_METHOD']);
